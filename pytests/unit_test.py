7c94b660bd169ee1cf623431c60bc12da723cce5
rlb32 <93962719+rlb32@users.noreply.github.com>
2023-03-21 20:28:14 -0500

    Update tests

--- pytests/unit_test.py
+++ pytests/unit_test.py
@@ -2,7 +2,7 @@ import BMI
 import pytest
 
 
-def test_BMI_Below_Min_Bound():
+def test_BMI_Under_Min_Bound():
     #Testing to see what BMI would output if the BMI was below the Underweight line
     output = BMI.BMI(76, 148.41)
     assert output == "Underweight"
@@ -13,12 +13,9 @@ def test_BMI_At_Min_Bound():
     output = BMI.BMI(76, 148.42)
     assert output == "Normal Weight"
 
-def test_BMI_Above_Min_Bound():
-    #Testing to see what BMI outputs when the BMI is above the Underweight line 
-    output = BMI.BMI(76, 148.43)
-    assert output == "Normal Weight"
 
-def test_BMI_Below_Max_Bound():
+
+def test_BMI_Under_Max_Bound():
     #Testing to see what BMI would output if the BMI was below the Obese line
     output = BMI.BMI(76, 240.66)
     assert output == "Overweight"
@@ -28,10 +25,19 @@ def test_BMI_At_Max_Bound():
     output = BMI.BMI(76, 240.67)
     assert output == "Obese"
 
-def test_BMI_Above_Max_Bound():
-    #Testing to see what BMI would output if the BMI was above the Obese line   
-    output = BMI.BMI(76, 240.68)
-    assert output == "Obese"
+    
+
+def test_BMI_Under_Middle_Bound():
+    #Testing to see what BMI would output if the BMI was below the Underweight line
+    output = BMI.BMI(76, 199.75)
+    assert output == "Normal Weight"
+
+
+def test_BMI__At_Middle_Bound():
+    #Testing to see what BMI outputs when the BMI is at the Underweight/Normal weight boundary
+    output = BMI.BMI(76, 199.76)
+    assert output == "Overweight"
+
 
 def test_Feet_To_Inches():
     #Not a boundary test but just a test to make sure the feet to inches function is working properly
